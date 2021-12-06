1dcf1652a0a2220dbdf8b6be3513f831ed0187bc
Parfait TOLEFO <78282359+parfaittolefo@users.noreply.github.com>
2021-12-06 21:12:32 +0100

    Update Scanner_of_ports.py

--- Scanner_of_ports.py
+++ Scanner_of_ports.py
@@ -1,29 +1,28 @@
 """
 Autor Parfait TOLEFO
 e-mail parfaittolefo23@gmail.com
-tel: +229 61474698/ +229 52422809
 """
 import socket,sys
-while 1:
+
+try:
+    site=sys.argv[1]
+    demande=sys.argv[2:]
+except:
+    print("Veuillez renseigner un nom de site valable\n")
+    sys.exit((1))
+if len(demande)==0:
+    demande=range(1,1000)
+if len(site)<=1:
+    print("Veuillez renseigner un nom de site valable\n")
+    sys.exit((1))  
+print("**************** Scannage en cours *******************")
+for ports in demande:      
     try:
-        site=sys.argv[1]
-        demande=sys.argv[2:]
-    except:
-        print("Veuillez renseigner un nom de site valable\n")
-        sys.exit((1))
-    if len(demande)==0:
-        demande=range(1,1000)
-    if len(site)<=1:
-        print("Veuillez renseigner un nom de site valable\n")
-        sys.exit((1))  
-    print("**************** Scannage en cours *******************")
-    for ports in demande:      
-        try:
-            ports=int(ports)
-            s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-            s.connect((site,ports))
-            print("\n{} --> ouvert".format(ports))
-            s.close()
-        except socket.error:
-            print("\n{} --> fermer".format(ports))
-            pass
+        ports=int(ports)
+        s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        s.connect((site,ports))
+        print("\nPort {} --> ouvert".format(ports))
+        s.close()
+    except socket.error:
+        print("\nPort {} --> fermer".format(ports))
+        pass
